7b84ba02767195136dd80b67ff4c4d1179b1248d
https://github.com/Amanueltamirat <amanuel.tamirat22@gmail.com>
2024-02-16 03:37:25 -0800

    ready to deploy

--- server-side/server.js
+++ server-side/server.js
@@ -39,7 +39,7 @@ const port = process.env.PORT || 3001;
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/client-side/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client-side/build/index.html"));
+  res.sendFile(path.join(__dirname, 'client-side', 'build', 'index.html'));
 });
 //server error
 app.use((err, req, res, next) => {
