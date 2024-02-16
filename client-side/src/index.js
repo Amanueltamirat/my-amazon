bf4e6d4d0abdb537b8826e52e1a7d42a811811ec
https://github.com/Amanueltamirat <amanuel.tamirat22@gmail.com>
2024-02-16 06:01:33 -0800

    new update

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -6,6 +6,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { StoreProvider } from "./Store";
+import {disableReactDevTools} from '@fvilers/disable-react-devtools'
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
